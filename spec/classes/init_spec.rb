c2c0629ca1d48693a7e2376e01d3eccb4fc2b61c
Bob Rentschler <bob.rentschler@gmail.com>
2014-06-13 17:08:36 -0500

    Rewrite rspec tests.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,35 +2,28 @@ require 'spec_helper'
 describe 'cachefilesd' do
 
   it { should compile.with_all_deps }
-
+  
   describe 'with default values for all parameters' do
+    let(:facts) { { :osfamily => 'RedHat' } }
+      
+    it { should contain_class('cachefilesd') }
+    
+    it {
+      should contain_file('cachefilesd.conf').with({
+        'ensure'  => 'file',
+        'path'    => '/etc/cachefilesd.conf',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+      })
+    }
+    
+    it {
+      should contain_file('nsswitch_config_file').with_content(%{"# This file maintained by puppet, do not edit.
 
-  describe 'packages' do
-    context 'with default params ' do
-      it do
-        should contain_package('cachefilesd').with({
-          'ensure' => 'installed',
-        })
-      end
-    end
-  end
-
-  describe 'config files' do
-    context 'with default params ' do
-      it {
-        should contain _file('/etc/cachefilesd.conf').with({
-          'ensure'  =>  'file',
-          'owner'   =>  '0',
-          'group'   =>  '0',
-          'mode'    =>  '0644',
-        })
-      }
-
-
-  it { should contain_file('/etc/cachefilesd.conf').with_content("# This file maintained by puppet, do not edit.
 dir /var/cache/fscache
 tag nfscache
-
+        
 # brun and frun represent the percent of free space (blocks) and free files available in the
 # filesystem the cache resides on required for cachefilesd to run freely. (no culling, files
 # added as accessed)
@@ -42,7 +35,7 @@ frun 10%
 # will start culling the cache (deleteing files)
 
 bcull 7%
-fcull 7%
+fcull 7%        
 
 # bstop and fstop represent the percent free on the file system at which point cachefilesd will
 # stop writing new files. It will resume writes only after the free space has risen to the
@@ -51,10 +44,7 @@ fcull 7%
 bstop 3%
 fstop 3%
 
-secctx system_u:system_r:cachefiles_kernel_t:s0
-")
-  }
-      end
-    end
-  end
-end
+secctx system_u:system_r:cachefiles_kernel_t:s0"})
+    }
+  end 
+end  
