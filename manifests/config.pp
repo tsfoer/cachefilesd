29473bb869bacab4c8102c13ff194a950662d34a
Bob Rentschler <bob.rentschler@gmail.com>
2014-06-13 17:17:23 -0500

    CHange file owner from 0 to 'root' for internal consistancy.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,8 @@
 class cachefilesd::config inherits cachefilesd {
   file { $config:
     ensure  => file,
-    owner   => 0,
-    group   => 0,
+    owner   => 'root',
+    group   => 'root',
     mode    => '0644',
     content => template('cachefilesd/cachefilesd.conf.erb'),
   }
